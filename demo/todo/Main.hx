1d577e8c28542d0d6b82138305e5d5a679cbd98f
Jason O'Neil <jason.oneil@gmail.com>
2012-04-11 16:43:07 +0800

    Created a TODO demo.  Very basic.
     * Works in Firefox, Chrome, Opera.
     * Does not work in IE9.  Presumably it's in my EventManagement code, which I haven't written ANY tests for yet.
     * Source code included (Only 57 lines including plenty of comments and whitespace!)
     * Compiled, mostly working version included.
    
    Fixes #14

---
+++ demo/todo/Main.hx
@@ -0,0 +1,57 @@
+using DOMTools;
+import DOMTools;
+
+class Main
+{
+	static public function main()
+	{
+		// Set up traces to go to the console
+		haxe.Log.trace = haxe.Firebug.trace;
+
+		// Run out app after the window has finished loading.
+		domtools.Query.window.onload = run;
+	}
+
+	static function run(e)
+	{
+		trace ("Welcome to my app!");
+
+		// Find the key parts of our app.
+		var form = "#inputform".find();
+		var input = "#task".find();
+		var tasks = "#tasks".find();
+		var clear = "#clear".find();
+
+		// On submit, add a new item to our list.
+		"#inputform".find().submit(function (e) {
+
+			// Get the value
+			var val = input.val();
+
+			// Create the list item, add it to the bottom
+			var li = "li".create().setText(val);
+			tasks.append(li);
+
+			// Reset the input field
+			input.setVal("");
+
+			// Prevent the page from going anywhere
+			e.preventDefault();
+		});
+
+		// When someone clicks on a task, toggle it
+		tasks.click(function (e) {
+			var target = e.target.toNode();
+			if (target.tagName() == "li")
+			{
+				target.toggleClass('completed');
+			}
+		});
+
+		// When someone clicks "Clear Completed", do it
+		clear.click(function (e) {
+			tasks.find(".completed").remove();
+		});
+
+	}
+}
