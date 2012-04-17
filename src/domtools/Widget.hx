f6edee99eb30bcce1180853362d352ea11cfa380
Jason O'Neil <jason.oneil@gmail.com>
2012-04-17 08:49:12 +0800

    Moved loadTemplate() macro into Widget.
    Had to use a fair bit of conditional compilation, but that's okay.

--- src/domtools/Widget.hx
+++ src/domtools/Widget.hx
@@ -29,14 +29,22 @@
 
 package domtools;
 
+#if js
 import js.w3c.level3.Core;
 import js.w3c.level3.Core.Document;
 import CommonJS;
 import domtools.Query;
 
-
 class Widget extends Query
+#else 
+import haxe.macro.Expr;
+import haxe.macro.Context;
+using StringTools;
+
+class Widget
+#end
 {
+	#if js
 	/** Create a new widget by parsing some "template" html, and use that as our collection.
 	When your class extends this, it will automatically work with the domtools API (through using).  
 	Therefore your class can have very tight integration between haxe features and the DOM. */
@@ -47,5 +55,51 @@ class Widget extends Query
 		var q = Query.parse(template);
 		this.collection = q.collection;
 	}
+	#end
 
+	/**
+	* Load a file at compile time to use as a template.  Filename should be relative to the classpath.
+	*/
+	#if (js || macro)
+	@:macro public static function loadTemplate( ?fileName : Expr )
+	{
+		var p = Context.currentPos();
+
+		// Get the name of the file passed to the macro
+		var templateFile = switch( fileName.expr ) 
+		{
+			case EConst(c):
+				switch( c ) 
+				{
+					case CString(str): str;
+					default: 
+						null;
+				}
+			default: null;
+		} 
+
+		// If no file was passed, look for a file in the same spot but with ".tpl"
+		// instead of ".hx" at the end.
+		if( templateFile == null ) {
+			var currentFile = Context.getPosInfos(p).file;
+			templateFile = ~/\.hx$/.replace(currentFile, ".tpl");
+		}
+
+		var f:String;
+
+		try 
+		{
+			// Try to read the specified file
+			f = neko.io.File.getContent(Context.resolvePath(templateFile));
+		} 
+		catch( e : Dynamic ) 
+		{
+			// If it fails, give an error message at compile time
+			var errorMessage = "Could not load template: " + templateFile;
+			Context.error(errorMessage, p);
+		}
+
+		return { expr : EConst(CString(f)), pos : p };
+	}
+	#end
 }
