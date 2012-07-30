277304f03dac367293152d449cad00500b610d24
Jason O'Neil <jason.oneil@gmail.com>
2012-07-30 23:52:38 +0800

    Major updates to get DataWidgets working.
    The basic idea now works.  See example/datawidget/DataWidgetExample.hx
    for what this is supposed to do.
    
    Highlights include:
    
     * Automatically import template at compile time
     * Automatically bind data to template
     * Compile time warnings if variables do not exist
     * Template has access to variables and functions of the object
     * String interpolation, so you can do
    
        <h1>Hello $name, how are you?</h1>
        <p>Posted at ${date.toString()}</p>
    
       Just like Std.format();

--- src/dtx/widget/WidgetTools.hx
+++ src/dtx/widget/WidgetTools.hx
@@ -1,8 +1,25 @@
+/****
+* Copyright (c) 2012 Jason O'Neil
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+****/
+
 package dtx.widget;
 
 import haxe.macro.Expr;
 import haxe.macro.Context;
+import haxe.macro.Type;
+import haxe.macro.Format;
+import tink.macro.tools.MacroTools;
+using tink.macro.tools.MacroTools;
 using StringTools;
+using Lambda;
+using Detox;
 
 class WidgetTools 
 {
@@ -14,22 +31,94 @@ class WidgetTools
     *  - Metadata in the form: @template("<div></div>") class MyWidget ...
     *  - Metadata in the form: @loadTemplate("MyWidgetTemplate.html") class MyWidget ...
     *  - Take a guess at a filename... use current filename, but replace ".hx" with ".html"
-    *  - 
+    * Once it finds it, it overrides the get_template() method, and makes it return
+    * the correct template as a String constant.  So each widget gets its own template
     */
     @:macro public static function buildWidget():Array<Field>
     {
-        //
-        // SET UP
-        //
         var widgetPos = Context.currentPos();                   // Position where the original Widget class is declared
         var localClass = haxe.macro.Context.getLocalClass();    // Class that is being declared
+        var fields = haxe.macro.Context.getBuildFields();
+
+        
+        // If get_template() already exists, don't recreate it
+        if (fields.exists(function (f) { return f.name == "get_template"; }) == false)
+        {
+            // Load the template
+            var template = loadTemplate(localClass);
+            
+            // Create and add the get_template() field.  
+            fields.push(createField_get_template(template, widgetPos));
+            return fields;
+        }
+        else
+        {
+            // Leave the fields as is
+            return null;
+        }
+    }
+
+    /**
+    * This build macro runs on every subclass of DataWidget.
+    * It trsfd
+    */
+    @:macro public static function buildDataWidget():Array<Field>
+    {
+        var p = Context.currentPos();                           // Position where the original Widget class is declared
+        var localClass = haxe.macro.Context.getLocalClass();    // Class that is being declared
+        var fields = haxe.macro.Context.getBuildFields();
+
+        var dataType = localClass.get().superClass.params[0];
+
+        // If get_template() already exists, don't recreate it
+        if (fields.exists(function (f) { return f.name == "get_template"; }) == false)
+        {
+            // Load the template
+            var template = loadTemplate(localClass);
+            var fnBody = new Array<Expr>();
+
+            // Collect all the statements we use
+            switch (dataType)
+            {
+                case TInst(t,params):
+                    var dataClass = t.get();
+                    for (expr in setupBindingVariables(dataClass))
+                    {
+                        fnBody.push(expr);
+                    }
+                default:
+                    haxe.macro.Context.error("DataWidget can only have a class instance as a type parameter", p);
+
+            }
+            var result = processTemplateBindings(template);
+            for (expr in result.bindings)
+            {
+                fnBody.push(expr);
+            }
+            
+            // Create the get_template() field and the refresh() field
+            fields.push(createField_refresh(fnBody));
+            fields.push(createField_get_template(result.template, p));
+        }
+
+
+        return fields;
+    }
+
+    /**
+      * Helper functions
+      */
+    #if macro
+
+    static function loadTemplate(localClass:Null<Ref<ClassType>>):String
+    {
         var p = localClass.get().pos;                           // Position where the original Widget class is declared
         var className = localClass.toString();                  // Name of the class eg "my.pack.MyType"
         var meta = localClass.get().meta;                       // Metadata of the this class
         
         var templateFile:String;                                // If we are loading template from a file, this is the filename
         var template:String = "";                               // If the template is directly in metadata, use that.
-        
+
         //
         // IF TEMPLATE IS DECLARED IN METADATA
         //
@@ -120,18 +209,16 @@ class WidgetTools
         }
 
         //
-        // SANE FALLBACK IN CASE SOMETHING BROKE
+        // IN CASE WE DON'T HAVE A SAFE TEMPLATE, FALLBACK TO <DIV>
         //
         if (template == null) template = "<div></div>";
 
-        
-        // If we wanted to manipulate fields, this is where to do it!
-        var fields = haxe.macro.Context.getBuildFields();
-
-        // add a new public class field named "hello" with type "Int"
-        //var tint = TPath({ pack : [], name : "Int", params : [], sub : null });
-        var position = haxe.macro.Context.currentPos();
-        fields.push({ 
+        return template;
+    }
+    
+    static function createField_get_template(template:String, widgetPos:Position):Field
+    {
+        return { 
             name : "get_template", 
             doc : null, 
             meta : [], 
@@ -147,67 +234,132 @@ class WidgetTools
                                     expr: EConst(
                                         CString(template)
                                     ), 
-                                    pos: position
+                                    pos: widgetPos
                                 }
                             ), 
-                            pos: position
+                            pos: widgetPos
                         }
                         ]
                     ), 
-                    pos: position
+                    pos: widgetPos
                 }, 
                 params: [], 
                 ret: null 
             }), 
-            pos: position
-        });
-
-            
-
-        return fields; 
+            pos: widgetPos
+        }
     }
 
-    /**
-    * Load a file at compile time to use as a template.  Filename should be relative to the classpath.
-    */
-    @:macro public static function loadTemplate( ?fileName : Expr ):Expr
+    static function setupBindingVariables(c:ClassType):Array<Expr>
     {
         var p = Context.currentPos();
+        var bindings = new Array();
 
-        // Get the name of the file passed to the macro
-        var templateFile = switch( fileName.expr ) 
+        for (f in c.fields.get())
         {
-            case EConst(c):
-                switch( c ) 
-                {
-                    case CString(str): str;
-                    default: 
-                        null;
-                }
-            default: null;
-        } 
-
-        // If no file was passed, look for a file in the same spot but with ".tpl"
-        // instead of ".hx" at the end.
-        if( templateFile == null ) {
-            var currentFile = Context.getPosInfos(p).file;
-            templateFile = ~/\.hx$/.replace(currentFile, ".tpl");
+            var varValue = ("o." + f.name).resolve();
+            var expr = f.name.define(varValue);
+            bindings.push(expr);
         }
 
-        var f:String;
-
-        try 
-        {
-            // Try to read the specified file
-            f = neko.io.File.getContent(Context.resolvePath(templateFile));
-        } 
-        catch( e : Dynamic ) 
-        {
-            // If it fails, give an error message at compile time
-            var errorMessage = "Could not load template: " + templateFile;
-            Context.error(errorMessage, p);
-        }
-
-        return macro "$f";
+        return bindings;
     }
+
+    static function processTemplateBindings(template:String):{ template:String, bindings:Array<Expr>}
+    {
+        // Get every node (including descendants)
+        var p = haxe.macro.Context.currentPos();
+        var xml = template.parse();
+        var allNodes = new dtx.DOMCollection();
+        allNodes.addCollection(xml);
+        allNodes.addCollection(xml.descendants(false));
+
+        /*
+        - for each loop
+          - if not set, set attr(data-loop-id, t); t++;
+          - take HTML, and create a new subwidget (as a class? or use a factory? eg PersonWidget.newLoop(1))
+          - get the type parameter (T) of the Iterable.  eg String in Array<String>
+          - create a class for the subwidget, take html
+          - recurse on this macro for the subwidget.
+        */
+
+        /*
+        - for each attribute, text node
+          - search for "$" (but not "$$")
+          - if found
+              - if not set, set attr(data-bind, i); i++;
+              - do string interpolation using modified Std.format(), get ("mailto:" + email)
+              - add binding to bind(), using find([data-bind=i]) and setAttr() or setText()
+        */
+
+        var bindingExpressions = new Array<Expr>();
+
+        var t = 0;
+        for (node in allNodes)
+        {
+            t++;
+            switch (node.nodeType)
+            {
+                case Xml.Element:
+                    // Set up bindings for each attribute value
+                    for (att in node.attributes())
+                    {
+                        var str = node.attr(att);
+                        if (str.indexOf('$') > -1)
+                        {
+                            node.setAttr('data-binding',Std.string(t));
+                            var stringAsExpr = Context.makeExpr(str, Context.currentPos());
+                            var nameAsExpr = Context.makeExpr(att, Context.currentPos());
+                            var identifierAsExpr = Context.makeExpr(Std.string(t), Context.currentPos());
+                            var interpolationExpr = Format.format(stringAsExpr);
+                            var bindingExpr = macro this.find('[data-binding="' + $identifierAsExpr + '"]').setAttr($nameAsExpr, $interpolationExpr);
+                            bindingExpressions.push(bindingExpr);
+                        }
+                    }
+                default:
+                    // Set up bindings for the text content...
+                    var str = node.text();
+                    if (str.indexOf('$') > -1)
+                    {
+                        // Will need to get nearest element + index, so we can do:
+                        // "nearestElm".find().children(false).get(index).setText();
+                        var parent = node.parent;
+                        if (parent.exists('data-binding') == false)
+                        {
+                            parent.setAttr('data-binding', Std.string(t));
+                        }
+                        var indexOfTextNode = Lambda.indexOf(parent.children(false), node);
+
+                        var stringAsExpr = Context.makeExpr(str, Context.currentPos());
+                        var identifierAsExpr = Context.makeExpr(parent.attr('data-binding'), Context.currentPos());
+                        var indexAsExpr = Context.makeExpr(indexOfTextNode, Context.currentPos());
+                        var interpolationExpr = Format.format(stringAsExpr);
+                        var bindingExpr = macro this.find('[data-binding="' + $identifierAsExpr + '"]').getNode($indexAsExpr).setText($interpolationExpr);
+                        bindingExpressions.push(bindingExpr);
+                    }
+            }
+        }
+
+        return { template: xml.html(), bindings: bindingExpressions };
+    }
+    
+    static function createField_refresh(fnBody:Array<Expr>):Field
+    {
+        var pos = Context.currentPos();
+        return { 
+            name : "refresh", 
+            doc : null, 
+            meta : [], 
+            access : [AOverride, APublic], 
+            kind : FFun({ 
+                args: [], 
+                expr: fnBody.toBlock(), 
+                params: [], 
+                ret: null 
+            }), 
+            pos: pos
+        }
+    }
+
+    #end 
 }
