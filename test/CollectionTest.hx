f7062ccd989f517662127ed923a75cf33ac375b3
Jason O'Neil <jason.oneil@gmail.com>
2012-09-29 23:20:29 +0800

    Updated unit tests to run on CPP
    Thanks to munit 9.5.2.  Had to change one thing in a unit test to get it
    to compile (Int can't be null on CPP), and then ran the tests.
    
    362 tests pass, 2 fail - both to do with the XML parser and comments, so
    will need to look into it...

--- test/CollectionTest.hx
+++ test/CollectionTest.hx
@@ -488,7 +488,7 @@ class CollectionTest
 	public function filterCallbackReturnsNull()
 	{
 		var filteredList = listItems.filter(function (li) {
-			#if flash9
+			#if (flash9 || cpp)
 			var returnValue = false;
 			#else
 			var returnValue:Bool = null;
