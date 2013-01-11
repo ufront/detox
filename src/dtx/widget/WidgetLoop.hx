5f5507de23cfc22b3fb95c395b266e16de9a3b76
Jason O'Neil <jason.oneil@gmail.com>
2013-01-11 16:07:38 +0800

    Allow autoPropMap - automatic mapping of properties
    I have set this as the on by default for objects - it seems reasonable

--- src/dtx/widget/WidgetLoop.hx
+++ src/dtx/widget/WidgetLoop.hx
@@ -9,13 +9,15 @@ class WidgetLoop<T> extends Loop<T>
 	var widgetClass:Class<dtx.widget.Widget>;
 	var propName:String;
 	var propMap:Dynamic<String>;
+	var autoPropMap:Bool;
 
-	public function new(widgetClass:Class<dtx.widget.Widget>, ?propName:String = null, ?propMap:Dynamic<String> = null)
+	public function new(widgetClass:Class<dtx.widget.Widget>, ?propName:String = null, ?propMap:Dynamic<String> = null, ?autoPropMap = true)
 	{
 		super();
 		this.widgetClass = widgetClass;
 		this.propName = propName;
 		this.propMap = propMap;
+		this.autoPropMap = autoPropMap;
 	}
 
 	override function generateItem(input:T):LoopItem<T>
@@ -36,6 +38,31 @@ class WidgetLoop<T> extends Loop<T>
 				var templateVar = Reflect.field(propMap, modelVar);
 				var modelValue = Reflect.getProperty(input, modelVar);
 				Reflect.setProperty(w, templateVar, modelValue);
+			}
+		}
+		if (autoPropMap)
+		{
+			var fieldNames:Array<String>;
+			switch (Type.typeOf(input))
+			{
+				case TObject:
+					// Anonymous object, use Reflect.fields()
+					fieldNames = Reflect.fields(input);
+				case TClass(c):
+					// Class instance, use Type.getInstanceFields()
+					fieldNames = Type.getInstanceFields(c);
+				default:
+					// This is not an object, so don't do property mapping
+					fieldNames = [];
+			}
+
+			for (fieldName in fieldNames)
+			{
+				if (Reflect.hasField(w, fieldName))
+				{
+					var modelValue = Reflect.getProperty(input, fieldName);
+					Reflect.setProperty(w, fieldName, modelValue);
+				}
 			}
 		}
 		return new LoopItem(input, w);
