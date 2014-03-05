1f180e00d76d67170adab0935e26b301de051e47
Jason O'Neil <jason.oneil@gmail.com>
2014-03-05 14:48:42 +0800

    Update to layout to reflect widget behaviour
    dtx-name="" elements now are exposed as DOMNode, not DOMCollection

--- src/dtx/layout/IDetoxLayout.hx
+++ src/dtx/layout/IDetoxLayout.hx
@@ -9,16 +9,16 @@ interface IDetoxLayout
 		public var title(default,set):String;
 
 		/** This is a contentContainer that you can empty and fill with new content **/
-		public var contentContainer(get,null):DOMCollection;
+		public var contentContainer(get,null):DOMNode;
 		
 		/** This is the `<html>` element or the document node.  Used as a workaround until we have a toHtml() method... that we can have in the interface **/
-		public var document(get,null):DOMCollection;
+		public var document(get,null):DOMNode;
 		
 		/** This is the `<head>` element on the page.  We append stylesheets to this. **/
-		public var head(get,null):DOMCollection;
+		public var head(get,null):DOMNode;
 
 		/** This is the `<body>` element on the page.  We insert our scripts _after_ this element. **/
-		public var body(get,null):DOMCollection;
+		public var body(get,null):DOMNode;
 	#end
 }
 
