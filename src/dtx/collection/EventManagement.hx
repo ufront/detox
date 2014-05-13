133036de0e9675abb279fa45386b2742ddcbcf9b
Jason O'Neil <jason.oneil@gmail.com>
2014-05-13 09:29:45 +0800

    Added documentation for EventManagement helper classes.
    Also removed the "ready" function.  Detox.ready() is where it's at.

--- src/dtx/collection/EventManagement.hx
+++ src/dtx/collection/EventManagement.hx
@@ -22,191 +22,231 @@ import Bean;
 	typedef DtxEvent = BnEvent;
 #end
 
+/**
+	This class provides static helper methods to listen to or trigger events for all nodes in a `dtx.DOMCollection`.
+
+	This class is intended to be used with static extension, by placing `using Detox;` in the imports for your module.
+	Each of these methods will then operate on DOMCollections as if they were methods on the DOMCollection object itself.
+	Each method is chainable, and returns the original collection with the original type information.
+	Each method is null-safe, if a collection is empty or null it will have no effect.
+**/
 class EventManagement
 {
+	/** Run `dtx.single.EventManagement.trigger` for each node in the target collection. **/
 	public static function trigger<T:DOMCollection>(targetCollection:T, eventType:String):T
 	{
-		for (target in targetCollection)
+		if (targetCollection!=null) for (target in targetCollection)
 		{
 			dtx.single.EventManagement.trigger(target, eventType);
 		}
 		return targetCollection;
 	}
 
+	/** Run `dtx.single.EventManagement.on` for each node in the target collection. **/
 	public static function on<T:DOMCollection>(targetCollection:T, eventType:String, ?selector:String, ?listener:EventListener):T
 	{
-		for (target in targetCollection)
+		if (targetCollection!=null) for (target in targetCollection)
 		{
 			dtx.single.EventManagement.on(target, eventType, selector, listener);
 		}
 		return targetCollection;
 	}
 
+	/** Run `dtx.single.EventManagement.off` for each node in the target collection. **/
 	public static function off<T:DOMCollection>(targetCollection:T, ?eventType:String, ?listener:EventListener):T
 	{
-		for (target in targetCollection)
+		if (targetCollection!=null) for (target in targetCollection)
 		{
 			dtx.single.EventManagement.off(target, eventType, listener);
 		}
 		return targetCollection;
 	}
 
+	/** Run `dtx.single.EventManagement.one` for each node in the target collection. **/
 	public static function one<T:DOMCollection>(targetCollection:T, eventType:String, ?selector:String, listener:EventListener):T
 	{
-		for (target in targetCollection)
+		if (targetCollection!=null) for (target in targetCollection)
 		{
 			dtx.single.EventManagement.one(target, eventType, selector, listener);
 		}
 		return targetCollection;
 	}
 
+	/** A shortcut for `on(target, "mousedown", selector, listener)`. **/
 	public static inline function mousedown<T:DOMCollection>(target:T, ?selector:String, ?listener:MouseEvent->Void):T
 	{
 		return on(target, "mousedown", selector, untyped listener);
 	}
 
+	/** A shortcut for `on(target, "mouseenter", selector, listener)`. **/
 	public static inline function mouseenter<T:DOMCollection>(target:T, ?selector:String, ?listener:MouseEvent->Void):T
 	{
 		return on(target, "mouseenter", selector, untyped listener);
 	}
 
+	/** A shortcut for `on(target, "mouseleave", selector, listener)`. **/
 	public static inline function mouseleave<T:DOMCollection>(target:T, ?selector:String, ?listener:MouseEvent->Void):T
 	{
 		return on(target, "mouseleave", selector, untyped listener);
 	}
 
+	/** A shortcut for `on(target, "mousemove", selector, listener)`. **/
 	public static inline function mousemove<T:DOMCollection>(target:T, ?selector:String, ?listener:MouseEvent->Void):T
 	{
 		return on(target, "mousemove", selector, untyped listener);
 	}
 
+	/** A shortcut for `on(target, "mouseout", selector, listener)`. **/
 	public static inline function mouseout<T:DOMCollection>(target:T, ?selector:String, ?listener:MouseEvent->Void):T
 	{
 		return on(target, "mouseout", selector, untyped listener);
 	}
 
+	/** A shortcut for `on(target, "mouseover", selector, listener)`. **/
 	public static inline function mouseover<T:DOMCollection>(target:T, ?selector:String, ?listener:MouseEvent->Void):T
 	{
 		return on(target, "mouseover", selector, untyped listener);
 	}
 
+	/** A shortcut for `on(target, "mouseup", selector, listener)`. **/
 	public static inline function mouseup<T:DOMCollection>(target:T, ?selector:String, ?listener:MouseEvent->Void):T
 	{
 		return on(target, "mouseup", selector, untyped listener);
 	}
 
+	/** A shortcut for `on(target, "keydown", selector, listener)`. **/
 	public static inline function keydown<T:DOMCollection>(target:T, ?selector:String, ?listener:KeyboardEvent->Void):T
 	{
 		return on(target, "keydown", selector, untyped listener);
 	}
 
+	/** A shortcut for `on(target, "keypress", selector, listener)`. **/
 	public static inline function keypress<T:DOMCollection>(target:T, ?selector:String, ?listener:KeyboardEvent->Void):T
 	{
 		return on(target, "keypress", selector, untyped listener);
 	}
 
+	/** A shortcut for `on(target, "keyup", selector, listener)`. **/
 	public static inline function keyup<T:DOMCollection>(target:T, ?selector:String, ?listener:KeyboardEvent->Void):T
 	{
 		return on(target, "keyup", selector, untyped listener);
 	}
 
+	/** Run `dtx.single.EventManagement.hover` for each node in the target collection. **/
 	public static function hover<T:DOMCollection>(targetCollection:T, ?selector: String, listener1:MouseEvent->Void, ?listener2:MouseEvent->Void):T
 	{
-		for (node in targetCollection)
+		if (targetCollection!=null) for (node in targetCollection)
 		{
 			dtx.single.EventManagement.hover(node, selector, listener1, listener2);
 		}
 		return targetCollection;
 	}
 
+	/** A shortcut for `on(target, "submit", selector, listener)`. **/
 	public static inline function submit<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "submit", selector, listener);
 	}
 
+	/** Run `dtx.single.EventManagement.toggleClick` for each node in the target collection. **/
 	public static function toggleClick<T:DOMCollection>(targetCollection:T, ?selector: String, listenerFirstClick:MouseEvent->Void, listenerSecondClick:MouseEvent->Void):T
 	{
-		for (target in targetCollection)
+		if (targetCollection!=null) for (target in targetCollection)
 		{
 			dtx.single.EventManagement.toggleClick(target, selector, listenerFirstClick, listenerSecondClick);
 		}
 		return targetCollection;
 	}
 
+	/** A shortcut for `on(target, "blur", selector, listener)`. **/
 	public static inline function blur<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "blur", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "change", selector, listener)`. **/
 	public static inline function change<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "change", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "click", selector, listener)`. **/
 	public static inline function click<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "click", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "dblclick", selector, listener)`. **/
 	public static inline function dblclick<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "dblclick", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "focus", selector, listener)`. **/
 	public static inline function focus<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "focus", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "focusIn", selector, listener)`. **/
 	public static inline function focusIn<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "focusIn", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "focusOut", selector, listener)`. **/
 	public static inline function focusOut<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "focusOut", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "resize", selector, listener)`. **/
 	public static inline function resize<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "resize", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "scroll", selector, listener)`. **/
 	public static inline function scroll<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "scroll", selector, listener);
 	}
 
+	/** Run `dtx.single.EventManagement.wheel` for each node in the target collection. **/
 	public static function wheel<T:DOMCollection>(target:T, ?selector:String, ?listener:WheelEvent->Void):T
 	{
-		for (n in target)
+		if (target!=null) for (n in target)
 		{
 			dtx.single.EventManagement.wheel(n, selector, listener);
 		}
 		return target;
 	}
 
+	/** A shortcut for `on(target, "select", selector, listener)`. **/
 	public static inline function select<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "select", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "load", selector, listener)`. **/
 	public static inline function load<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "load", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "unload", selector, listener)`. **/
 	public static inline function unload<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "unload", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "error", selector, listener)`. **/
 	public static inline function error<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "error", selector, listener);
 	}
 
+	/** A shortcut for `on(target, "ready", selector, listener)`. **/
 	public static inline function ready<T:DOMCollection>(target:T, ?selector:String, ?listener:EventListener):T
 	{
 		return on(target, "ready", selector, listener);
--- src/dtx/single/EventManagement.hx
+++ src/dtx/single/EventManagement.hx
@@ -14,24 +14,64 @@ package dtx.single;
 import dtx.DOMNode;
 import js.html.*;
 
+/**
+	This class provides static helper methods to listen to or trigger events for a DOMNode.
+
+	This class is intended to be used with static extension, by placing `using Detox;` in the imports for your module.
+	Each of these methods will then operate on a DOMNode as if they were methods of the DOMNode object itself.
+	Each method is chainable, and returns the original DOMNode.
+	Each method is null-safe, if a node is null or of the wrong type it will have no effect.
+**/
 class EventManagement
 {
-	/** Trigger an event, as if it has actually happened */
-	public static inline function trigger(target:DOMNode, eventString:String):DOMNode
+	/**
+		Trigger an event as if it occured on this node.
+
+		Custom event names can be used.
+		Several functions in the `EventManagement` class can provide a shortcut to a common event trigger, for example calling `myNode.click()` is the same as calling `EventManagement.trigger(myNode,"click")`.
+		The implementation uses `Bean.fire` to provide a consistent cross-browser implementation.
+		On targets other than Javascript this method has no effect.
+
+		@param target The node to use as the event target.  If `target` is null no event will be triggered.
+		@param eventName The name of the event to trigger.  If `eventName` is null no event will be triggered.
+		@return The target node.
+	**/
+	public static inline function trigger(target:DOMNode, eventName:String):DOMNode
 	{
 		#if js 
-		if (target!=null) Bean.fire(target, eventString);
-		#else 
-		trace ("Detox events only work on the Javascript target, sorry.");
+			if (target!=null && eventName!=null) Bean.fire(target, eventName);
 		#end
 		return target;
 	}
 
-	/** add an event listener */
+	/**
+		Add an event listener to a node.
+
+		The listener can optionally apply only to certian child nodes matching a selector.
+		If no selector is specified, the listener will be triggered for the given event type on this node and any child nodes.
+
+		Please note that if `listener` is null, this will currently have the effect of calling `trigger(target,eventType)`, but this is an implementation detail and may change in future - you should not rely on this behaviour.
+
+		Currently this is implemented with BeanJS, the code is either:
+
+		- `Bean.on(target,eventType,selector,listener);` if a selector is defined, or
+		- `Bean.on(target,eventType,listener);` if a selector is not defined.
+
+		In future we may change to using standard DOM methods rather than Bean, and drop support for old Internet Explorer browsers.
+
+		This method has no effect on targets other than Javascript.
+
+		@param target The DOMNode to add the listener to.  If null, this method will have no effect.
+		@param eventType The name of the event to listen to, for example, "click".  If null, this method will have no effect.
+		@param selector An optional selector - the listener will only be triggered if the event was on a child node which matches the selector.
+                        For example `on(document,"click","a",listener)` will trigger for all clicks on `<a>` elements inside the document.
+		@param listener The event listener function.  It should take a single parameter containing the current event.
+		@return The original DOMNode.
+	**/
 	public static function on(target:DOMNode, eventType:String, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		#if js 
-			if (target != null)
+			if (target != null && eventType != null)
 			{
 				if (listener != null)
 				{
@@ -40,12 +80,28 @@ class EventManagement
 				}
 				else trigger (target, eventType);
 			}
-		#else 
-			trace ("Detox events only work on the Javascript target, sorry.");
 		#end
 		return target;
 	}
 
+	/**
+		Remove an event listener from a node.
+
+		- If only `listener` is specified, that specific listener will be removed on all event types.
+		- If only `eventType` is specified, all listeners for that event type will be removed.
+		- If both `listener` and `eventType` are specified, only that exact listener on that event type will be removed.
+		- If neither `listener` or `eventType` are specified, then all event listeners on that node will be removed.
+
+		Currently this is implemented with BeanJS, using `Bean.off()` with the given arguments.
+		In future we may change to using standard DOM methods rather than Bean, and drop support for old Internet Explorer browsers.
+
+		This method has no effect on targets other than Javascript.
+
+		@param target The DOMNode to add the listener to.  If null, this method will have no effect.
+		@param eventType The name of the event to listen to, for example, "click".  If null, this method will have no effect.
+		@param listener The event listener function.  It should take a single parameter containing the current event.
+		@return The original DOMNode.
+	**/
 	public static function off(target:DOMNode, ?eventType:String, ?listener:EventListener):DOMNode
 	{
 		#if js 
@@ -56,13 +112,34 @@ class EventManagement
 				else if (listener != null) Bean.off(target, listener);
 				else Bean.off(target);
 			}
-		#else 
-			trace ("Detox events only work on the Javascript target, sorry.");
 		#end
 		return target;
 	}
 
-	/** Attach an event but only let it run once */
+	/**
+		Add an event listener to a node, but remove it after the first time it has been triggered.
+
+		The event listener will only be called once.
+
+		The listener can optionally apply only to certian child nodes matching a selector.
+		If no selector is specified, the listener will be triggered for the given event type on this node and any child nodes.
+
+		Currently this is implemented with BeanJS, the code is either:
+
+		- `Bean.one(target,eventType,selector,listener);` if a selector is defined, or
+		- `Bean.one(target,eventType,listener);` if a selector is not defined.
+
+		In future we may change to using standard DOM methods rather than Bean, and drop support for old Internet Explorer browsers.
+
+		This method has no effect on targets other than Javascript.
+
+		@param target The DOMNode to add the listener to.  If null, this method will have no effect.
+		@param eventType The name of the event to listen to, for example, "click".  If null, this method will have no effect.
+		@param selector An optional selector - the listener will only be triggered if the event was on a child node which matches the selector.
+                        For example `on(document,"click","a",listener)` will trigger for all clicks on `<a>` elements inside the document.
+		@param listener The event listener function.  It should take a single parameter containing the current event.
+		@return The original DOMNode.
+	**/
 	public static function one(target:DOMNode, eventType:String, ?selector:String, listener:EventListener):DOMNode
 	{
 		#if js 
@@ -71,62 +148,79 @@ class EventManagement
 				if (selector != null) Bean.one(target, eventType, selector, listener);
 				else Bean.one(target, eventType, listener);
 			}
-		#else 
-			trace ("Detox events only work on the Javascript target, sorry.");
 		#end
 		return target;
 	}
 
+	/** A typed shortcut for `on(target,"mousedown",selector,listener)`. **/
 	public static inline function mousedown(target:DOMNode, ?selector:String, ?listener:MouseEvent->Void):DOMNode
 	{
 		return on(target, "mousedown", selector, untyped listener);
 	}
 
+	/** A typed shortcut for `on(target,"mouseover",selector,listener)`. **/
 	public static inline function mouseenter(target:DOMNode, ?selector:String, ?listener:MouseEvent->Void):DOMNode
 	{
 		return on(target, "mouseover", selector, untyped listener);
 	}
 
+	/** A typed shortcut for `on(target,"mouseout",selector,listener)`. **/
 	public static inline function mouseleave(target:DOMNode, ?selector:String, ?listener:MouseEvent->Void):DOMNode
 	{
 		return on(target, "mouseout", selector, untyped listener);
 	}
 
+	/** A typed shortcut for `on(target,"mousemove",selector,listener)`. **/
 	public static inline function mousemove(target:DOMNode, ?selector:String, ?listener:MouseEvent->Void):DOMNode
 	{
 		return on(target, "mousemove", selector, untyped listener);
 	}
 
+	/** A typed shortcut for `on(target,"mouseout",selector,listener)`. **/
 	public static inline function mouseout(target:DOMNode, ?selector:String, ?listener:MouseEvent->Void):DOMNode
 	{
 		return on(target, "mouseout", selector, untyped listener);
 	}
 
+	/** A typed shortcut for `on(target,"mouseover",selector,listener)`. **/
 	public static inline function mouseover(target:DOMNode, ?selector:String, ?listener:MouseEvent->Void):DOMNode
 	{
 		return on(target, "mouseover", selector, untyped listener);
 	}
 
+	/** A typed shortcut for `on(target,"mouseup",selector,listener)`. **/
 	public static inline function mouseup(target:DOMNode, ?selector:String, ?listener:MouseEvent->Void):DOMNode
 	{
 		return on(target, "mouseup", selector, untyped listener);
 	}
 
+	/** A typed shortcut for `on(target,"keydown",selector,listener)`. **/
 	public static inline function keydown(target:DOMNode, ?selector:String, ?listener:KeyboardEvent->Void):DOMNode
 	{
 		return on(target, "keydown", selector, untyped listener);
 	}
 
+	/** A typed shortcut for `on(target,"keypress",selector,listener)`. **/
 	public static inline function keypress(target:DOMNode, ?selector:String, ?listener:KeyboardEvent->Void):DOMNode
 	{
 		return on(target, "keypress", selector, untyped listener);
 	}
 
+	/** A typed shortcut for `on(target,"keyup",selector,listener)`. **/
 	public static inline function keyup(target:DOMNode, ?selector:String, ?listener:KeyboardEvent->Void):DOMNode
 	{
 		return on(target, "keyup", selector, untyped listener);
 	}
 
+	/**
+		Trigger a listener for when a mouse hovers over an element, and again for when it leaves an element.
+
+		@param target The DOMNode to listen to.
+		@param selector An optional selector to listen to.
+		@listener1 A listener for the "mouseenter" event.  If `listener2` is not specified it will also be used for the "mouseleave" event.
+		@listener2 A listener for the "mouseleave" event.  If not specified, then `listener1` will trigger on "mouseleave" also.
+		@return The original DOMNode.
+	**/
 	public static function hover(target:DOMNode, ?selector:String, listener1:MouseEvent->Void, ?listener2:MouseEvent->Void):DOMNode
 	{
 		mouseenter(target, selector, listener1);
@@ -144,11 +238,23 @@ class EventManagement
 		return target;
 	}
 
+	/** A shortcut for `on(target,"submit",selector,listener)`. **/
 	public static inline function submit(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "submit", selector, listener);
 	}
 
+	/**
+		Trigger a listener for alternating clicks on an element.
+
+		The first click will trigger `listenerFirstClick`, the second will trigger `listenerSecondClick`, the third `listenerFirstClick` and so on.
+
+		@param target The DOMNode to listen to.
+		@param selector An optional selector to listen to.
+		@listenerFirstClick A listener for the first "click" event.
+		@listenerSecondClick A listener for the second "click" event.
+		@return The original DOMNode.
+	**/
 	public static function toggleClick(target:DOMNode, ?selector:String, listenerFirstClick:MouseEvent->Void, listenerSecondClick:MouseEvent->Void):DOMNode
 	{
 		// Declare and initialise now so they can reference each other in their function bodies.
@@ -177,51 +283,67 @@ class EventManagement
 		return target;
 	}
 
+	/** A shortcut for `on(target,"blur",selector,listener)`. **/
 	public static inline function blur(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "blur", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"change",selector,listener)`. **/
 	public static inline function change(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "change", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"click",selector,listener)`. **/
 	public static inline function click(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "click", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"dblclick",selector,listener)`. **/
 	public static inline function dblclick(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "dblclick", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"focus",selector,listener)`. **/
 	public static inline function focus(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "focus", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"focusIn",selector,listener)`. **/
 	public static inline function focusIn(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "focusIn", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"focusOut",selector,listener)`. **/
 	public static inline function focusOut(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "focusOut", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"resize",selector,listener)`. **/
 	public static inline function resize(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "resize", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"scroll",selector,listener)`. **/
 	public static inline function scroll(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "scroll", selector, listener);
 	}
 
+	/**
+		Add a listener to the wheel event.
+
+		Please note this implementation is incomplete - selector will have no effect, and it is not guaranteed to work cross browser.
+
+		Pull requests are welcome.
+	**/
 	public static function wheel(target:DOMNode, ?selector:String, ?listener:js.html.WheelEvent->Void):DOMNode
 	{
 		// Just use the HTML5 standard for now.  Works in IE9+ and FF17+, probably not webkit/opera yet.
@@ -232,29 +354,27 @@ class EventManagement
 		// https://developer.mozilla.org/en-US/docs/Mozilla_event_reference/wheel?redirectlocale=en-US&redirectslug=DOM%2FDOM_event_reference%2Fwheel
 	}
 
+	/** A shortcut for `on(target,"select",selector,listener)`. **/
 	public static inline function select(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "select", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"load",selector,listener)`. **/
 	public static inline function load(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "load", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"unload",selector,listener)`. **/
 	public static inline function unload(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "unload", selector, listener);
 	}
 
+	/** A shortcut for `on(target,"error",selector,listener)`. **/
 	public static inline function error(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
 	{
 		return on(target, "error", selector, listener);
 	}
-
-	public static inline function ready(target:DOMNode, ?selector:String, ?listener:EventListener):DOMNode
-	{
-		return on(target, "ready", selector, listener);
-	}
-
 }
