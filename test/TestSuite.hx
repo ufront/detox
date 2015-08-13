cd1a0e81bec6471a3a7ddd0a25bd8d36b824e3b4
Jason O'Neil <jason.oneil@gmail.com>
2015-08-13 15:55:23 +1000

    Munit deciding things need to be in a different order.

--- test/TestSuite.hx
+++ test/TestSuite.hx
@@ -1,21 +1,21 @@
 import massive.munit.TestSuite;
 
-import AnimationTest;
 import CollectionAnimationTest;
-import ElementManipulationTest;
-import StyleTest;
-import CollectionStyleTest;
-import TraversingTest;
-import DOMManipulationTest;
-import CollectionEventManagementTest;
 import LoopTest;
-import EventManagementTest;
-import CollectionElementManipulationTest;
-import CollectionTraversingTest;
 import ToolsTest;
+import EventManagementTest;
+import TraversingTest;
+import CollectionTraversingTest;
 import CollectionTest;
-import CollectionDOMManipulationTest;
 import WidgetTest;
+import ElementManipulationTest;
+import CollectionEventManagementTest;
+import CollectionStyleTest;
+import CollectionElementManipulationTest;
+import AnimationTest;
+import CollectionDOMManipulationTest;
+import StyleTest;
+import DOMManipulationTest;
 
 /**
  * Auto generated Test Suite for MassiveUnit.
@@ -29,21 +29,21 @@ class TestSuite extends massive.munit.TestSuite
 	{
 		super();
 
-		add(AnimationTest);
 		add(CollectionAnimationTest);
-		add(ElementManipulationTest);
-		add(StyleTest);
-		add(CollectionStyleTest);
-		add(TraversingTest);
-		add(DOMManipulationTest);
-		add(CollectionEventManagementTest);
 		add(LoopTest);
-		add(EventManagementTest);
-		add(CollectionElementManipulationTest);
-		add(CollectionTraversingTest);
 		add(ToolsTest);
+		add(EventManagementTest);
+		add(TraversingTest);
+		add(CollectionTraversingTest);
 		add(CollectionTest);
-		add(CollectionDOMManipulationTest);
 		add(WidgetTest);
+		add(ElementManipulationTest);
+		add(CollectionEventManagementTest);
+		add(CollectionStyleTest);
+		add(CollectionElementManipulationTest);
+		add(AnimationTest);
+		add(CollectionDOMManipulationTest);
+		add(StyleTest);
+		add(DOMManipulationTest);
 	}
 }
